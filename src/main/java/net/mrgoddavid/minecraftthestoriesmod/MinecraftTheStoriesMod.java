535517d24afde18b4bcbac053df25211d2660874
MrGodDavid <davidbodongliu@gmail.com>
2026-08-11 21:38:43 -0400

    Initial commit

---
+++ src/main/java/net/mrgoddavid/minecraftthestoriesmod/MinecraftTheStoriesMod.java
@@ -0,0 +1,30 @@
+package net.mrgoddavid.minecraftthestoriesmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.resources.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This is the main class of this mod development. I am pretty excited because today is the first mo-dev. :)
+ * Hopefully I can survive the sea of Minecraft modding. LWJGL is insanely hard. Perhaps modding as hard as learning
+ * LWJGL 3...
+ *
+ * @author Mr. GodDavid
+ * @since 8/11/2026
+ */
+public class MinecraftTheStoriesMod implements ModInitializer {
+
+	public static final String MOD_ID = "minecraft-the-stories-mod";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
